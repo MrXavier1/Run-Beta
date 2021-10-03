80e593068db9d0c71a73f0779212c660dc9bc1fe
MrXavier1 <91851491+MrXavier1@users.noreply.github.com>
2021-10-03 23:49:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.Prefix = `!`;
-exports.Token = `your bot token`;
-exports.Color = `RANDOM`;
+exports.Prefix = `/`;
+exports.Token = 'ODkzNDU5MzA3MDQyMDU0MTc1.YVbw0g.hG7xVewxKlWO6TX10xBxCu56WvI';
+exports.Color = `RED`;
